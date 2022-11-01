5e0d4547ca36ecf4b70522514429eaf20ef1a8c5
Picadasgalaxias <28831265+Picadasgalaxias@users.noreply.github.com>
2022-11-01 19:35:07 -0400

    ok

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
-	"io"
+	"log"
 )
 
 func main() {
-	name := "test" // email
+	name := "email" // email
 
-	s := "3x8)*&$"
-	h := md5.New()
-	io.WriteString(h, s+name)
-	s = fmt.Sprintf("%X", h.Sum(nil))
-	s = s[:4] + "-" + s[4:]
-	fmt.Println(s[0:9])
+	hash := md5.New()
+	const s = "3x8)*&$"
+
+	_, err := hash.Write([]byte(s + name))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	key := hash.Sum(nil)
+	fmt.Printf("%X-%X", key[:2], key[2:4])
 }
